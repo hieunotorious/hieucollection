4fa7ee2a5e3f5d42391fcceac12439fd7e54e8ec
mangochieu <hieumn2001@gmail.com>
2022-12-18 20:43:37 +0700

    test

--- services/ProductService/index.ts
+++ services/ProductService/index.ts
@@ -2,18 +2,20 @@ import { ProductType } from "./../../api/auth/models/product";
 import axios from "axios";
 
 export const getProduct = async (offset?: number, limit?: number) => {
-  const product = await axios.get(
+  const product = await fetch(
     `${process.env.NEXT_PUBLIC_HOST}/product/all?offset=${offset || 0}&limit=${
       limit || 0
     }`
   );
-  return (product.data as ProductType[]) || undefined;
+  const data = await product.json();
+  return (data as ProductType[]) || undefined;
 };
 
 export const getProductId = async (id: string) => {
-  const product = await axios.get(
+  const product = await fetch(
     `${process.env.NEXT_PUBLIC_HOST}/product/get/${id}
     `
   );
-  return (product.data as ProductType) || undefined;
+  const data = await product.json();
+  return (data as ProductType) || undefined;
 };
